fb1b72bd0554314c608bb4e34e08d45032ab0398
minji-o-j <jminji98@gmail.com>
2020-06-08 02:55:53 +0900

    Create CommonVariables.java
    Co-Authored-By: 201810794_eunhoo <201810794@users.noreply.github.com>

---
+++ app/src/main/java/com/esrc/biosignal/commonutils/CommonVariables.java
@@ -0,0 +1,10 @@
+package com.esrc.biosignal.commonutils;
+
+public class CommonVariables {
+    public static double bpm1 = 0;
+    public static double bpm2 = 0;
+    public static double LF1=0;
+    public static double LF2=0;
+    public static double HF1=0;
+    public static double HF2=0;
+}
